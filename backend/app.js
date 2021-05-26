745cad45dfa734707a34602151eb4aa01fb4667d
Patrick Carter <49718153+Patrick-Carter@users.noreply.github.com>
2021-05-26 14:08:52 -0500

    added validators to signup,login route. and handled all errors

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,17 @@ server.use(express.json());
 
 server.use("/api/v1/user", userRoutes);
 
+// I need to catch any errors that happen
+server.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred" });
+});
+
 server.listen(5000, () => {
   console.log("server has started on port 5000");
 });
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const userController = require("../controllers/user-controller");
 
 const router = express.Router();
 
-router.post("/login", userController.loginUser);
+router.post(
+  "/login",
+  check("email").isEmail().normalizeEmail(),
+  userController.loginUser
+);
 
-router.post("/signup", userController.signup)
+router.post(
+  "/signup",
+  [
+    check("email").isEmail().normalizeEmail(),
+    check("password").isLength({ min: 5, max: 255 }).trim(),
+  ],
+  userController.signup
+);
 
 module.exports = router;
