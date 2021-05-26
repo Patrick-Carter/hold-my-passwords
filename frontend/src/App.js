65c6ea87c84dc2508e4ff4384692720db9d1d664
Patrick Carter <49718153+Patrick-Carter@users.noreply.github.com>
2021-05-26 16:42:44 -0500

    setup React Project

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"></Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
