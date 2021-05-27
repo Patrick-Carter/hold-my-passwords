c76e2c6cd2c93ceb1aebcd95a22ddd2a5a52a285
Patrick Carter <49718153+Patrick-Carter@users.noreply.github.com>
2021-05-27 14:21:24 -0500

    Revert "Revert "added mock for the server""
    This reverts commit c4d5994e5af2d2bea13e02444cb9ed67c1bc0dd0.

---
+++ frontend/src/pages/landing-page/TESTS/LandingPage.spec.js
@@ -0,0 +1,51 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import LandingPage from "../LandingPage";
+
+describe("Tests for LandingPage", () => {
+  test("should have header text 'Sign-up'", () => {
+    render(<LandingPage />);
+
+    const headerText = screen.getByRole("heading", { name: "Sign-up!" });
+
+    expect(headerText).toBeInTheDocument();
+  });
+
+  test("should have a button that says 'Switch To Login' ", () => {
+    render(<LandingPage />);
+    const headerText = screen.getByRole("heading", { name: "Sign-up!" });
+    const button = screen.getByRole("button", { name: "Switch To Login" });
+    const passwordConfirmInput = screen.getByRole("textbox", {
+      name: "Confirm Password:",
+    });
+
+    fireEvent.click(button);
+
+    expect(headerText.textContent).toBe("Login!");
+    expect(button.textContent).toBe("Switch To Sign-up");
+    expect(passwordConfirmInput).not.toBeInTheDocument();
+  });
+
+  test("should have a button that submits a signup request", async () => {
+    render(<LandingPage />);
+
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+
+    fireEvent(submitButton);
+
+  });
+
+  test("should have labels and inputs for email and password", () => {
+    render(<LandingPage />);
+
+    const emailLabel = screen.getByLabelText("Email:");
+    const emailInput = screen.getByRole("textbox", { name: "Email:" });
+
+    const passwordLabel = screen.getByLabelText("Password:");
+    const passwordInput = screen.getByRole("textbox", { name: "Password:" });
+
+    const passwordConfirmLabel = screen.getByLabelText("Confirm Password:");
+    const passwordConfirmInput = screen.getByRole("textbox", {
+      name: "Confirm Password:",
+    });
+  });
+});
